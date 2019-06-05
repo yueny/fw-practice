b0f471c528bff26d52d53a14df82b03d2ff4e2bb
yueny <yueny09@163.com>
2019-06-05 18:56:55 +0800

    lock

--- src/main/java/com/yueny/fw/practice/manager/IService.java
+++ src/main/java/com/yueny/fw/practice/manager/IService.java
@@ -1,29 +1,29 @@
-package com.yueny.fw.practice.manager;
-
-import com.yueny.fw.practice.bo.CarBo;
-import com.yueny.fw.practice.entry.CarEntry;
-import com.yueny.superclub.api.pojo.IBo;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.Mappings;
-import org.mapstruct.factory.Mappers;
-
-/**
- * <code>
- *
- * </code>
- *
- * @author yueny09 <deep_blue_yang@163.com>
- * @DATE 2019/6/5 下午6:02
- */
-@Mapper
-public interface IService {
-//    IService.INSTANCE
-    //public IService INSTANCE = Mappers.getMapper(IService.class);
-
-    @Mapping(source = "numberOfSeats", target = "seatCount")
-    CarBo carToCarDto(CarEntry car);
-
-//    public <T extends IBo> void mapper(CarEntry carDto, @MappingTarget T t);
-}
+//package com.yueny.fw.practice.manager;
+//
+//import com.yueny.fw.practice.bo.CarBo;
+//import com.yueny.fw.practice.entry.CarEntry;
+//import com.yueny.superclub.api.pojo.IBo;
+//import org.mapstruct.Mapper;
+//import org.mapstruct.Mapping;
+//import org.mapstruct.MappingTarget;
+//import org.mapstruct.Mappings;
+//import org.mapstruct.factory.Mappers;
+//
+///**
+// * <code>
+// *
+// * </code>
+// *
+// * @author yueny09 <deep_blue_yang@163.com>
+// * @DATE 2019/6/5 下午6:02
+// */
+////@Mapper
+//public interface IService {
+////    IService.INSTANCE
+//    //public IService INSTANCE = Mappers.getMapper(IService.class);
+//
+//    @Mapping(source = "numberOfSeats", target = "seatCount")
+//    CarBo carToCarDto(CarEntry car);
+//
+////    public <T extends IBo> void mapper(CarEntry carDto, @MappingTarget T t);
+//}
--- src/main/java/com/yueny/fw/practice/manager/impl/UserLoginManagerImpl.java
+++ src/main/java/com/yueny/fw/practice/manager/impl/UserLoginManagerImpl.java
@@ -6,7 +6,6 @@ package com.yueny.fw.practice.manager.impl;
 import com.yueny.fw.practice.bo.CarBo;
 import com.yueny.fw.practice.entry.CarEntry;
 import com.yueny.fw.practice.entry.CarType;
-import com.yueny.fw.practice.manager.IService;
 import com.yueny.fw.practice.manager.IUserLoginManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +15,8 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class UserLoginManagerImpl implements IUserLoginManager {
-	@Autowired
-	private IService service;
+//	@Autowired
+//	private IService service;
 
 	@Override
 	public boolean login(final String userName) {
@@ -26,9 +25,9 @@ public class UserLoginManagerImpl implements IUserLoginManager {
 		entry.setNumberOfSeats(2);
 		entry.setType(CarType.A);
 
-		CarBo bo = service.carToCarDto(entry);
+//		CarBo bo = service.carToCarDto(entry);
 
-		log.info("result is :{}.", bo);
+//		log.info("result is :{}.", bo);
 
 		return true;
 	}
