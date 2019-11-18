f88be139595a408356dda47be2ec715e281a4b52
yueny <yueny09@163.com>
2019-11-18 14:30:05 +0800

    线程池  doc

--- src/main/java/com/yueny/fw/practice/thread/ThreadPoolMain.java
+++ src/main/java/com/yueny/fw/practice/thread/ThreadPoolMain.java
@@ -14,6 +14,17 @@ import java.util.concurrent.TimeUnit;
  * @Date 2019-10-30 17:34
  */
 public class ThreadPoolMain {
+
+    private static final int COUNT_BITS = Integer.SIZE - 3;
+    private static final int CAPACITY   = (1 << COUNT_BITS) - 1;
+
+    // runState is stored in the high-order bits
+    private static final int RUNNING    = -1 << COUNT_BITS;
+    private static final int SHUTDOWN   =  0 << COUNT_BITS;
+    private static final int STOP       =  1 << COUNT_BITS;
+    private static final int TIDYING    =  2 << COUNT_BITS;
+    private static final int TERMINATED =  3 << COUNT_BITS;
+
     public static void main(String[] args) {
         /**
          * int corePoolSize,
@@ -30,9 +41,7 @@ public class ThreadPoolMain {
                 new LinkedBlockingDeque<>(300),
                 new ThreadFactoryBuilder().setNameFormat("demo").build(),
                 new ThreadPoolExecutor.AbortPolicy()
-                );
-
-
+        );
     }
 
 }
